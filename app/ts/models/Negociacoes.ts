55856ce9fcec5ebc6b069439e52fc5bfb702ebb3
igordev <igorth.genesis@gmail.com>
2020-07-31 13:56:17 -0300

    Atividade 04 Cap. 03

---
+++ app/ts/models/Negociacoes.ts
@@ -0,0 +1,13 @@
+class Negociacoes {
+
+    private _negociacoes:Negociacao[] = []
+    //private _negociacoes:Array<Negociacao> = []
+
+    adiciona(negociacao: Negociacao): void {
+        this._negociacoes.push(negociacao);
+    }
+
+    paraArray():Negociacao[] { //boa prática
+        return [].concat(this._negociacoes); //encapsulamento -> imutável
+    }
+}
