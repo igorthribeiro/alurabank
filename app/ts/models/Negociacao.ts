2cfcf7596b45ee78b209b409e5ec71ad8995c01f
igordev <igorth.genesis@gmail.com>
2020-08-08 07:05:47 -0300

    Atividade 01 Cap. 02

--- app/ts/models/Negociacao.ts
+++ app/ts/models/Negociacao.ts
@@ -3,7 +3,7 @@ export class Negociacao {
     //private _inputData:any; //quando o tipo não é definido
 
 
-    constructor(private _data: Date, private _quantidade: number, private _valor: number) {
+    constructor(readonly data: Date, readonly quantidade: number, readonly valor: number) {
         //o underline identifica (convenção) que os valor só poderão ser acessados por métodos
         
         /*
@@ -15,20 +15,8 @@ export class Negociacao {
 
     }
 
-    get data() {
-        return this._data;
-    }
-
-    get quantidade() {
-        return this._quantidade;
-    }
-
-    get valor() {
-        return this._valor;
-    }
-
     get volume() {
-        return this._quantidade * this._valor;
+        return this.quantidade * this.valor;
     }
 
 }
